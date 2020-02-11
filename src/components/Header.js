c538e9a832ebd6c037b147e6d38d295109cd7709
Nick <barnard.nicholas@googlemail.com>
2020-02-11 10:12:45 +0000

    add header details

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Nick Barnard</h1>
+      <div class="triple-diamond-deco-container container">
+        <div class="left-line"></div>
+        <div class="right-line"></div>
+        <div class="small-diamond-left"></div>
+        <div class="small-diamond-right"></div>
+        <div class="large-diamond"></div>
+      </div>
+      <div className="headersubtitle">
+        <h4>Full-Stack</h4>
+        <h4>Development</h4>
+      </div>
     </div>
   );
 };
