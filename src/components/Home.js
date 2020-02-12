155b1ffd73c51051bcabea166ab8edb25becc538
Nick <barnard.nicholas@googlemail.com>
2020-02-12 10:53:13 +0000

    add content to home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,23 @@ import PageTile from "./PageTile";
 const homeContent = () => {
   return (
     <>
-      <h3>About Me</h3>
+      <h3>Who Am I?</h3>
       <p>
-        Junior Full-Stack Developer, based in Manchester, UK. This page serves
-        as my development portfolio, so please feel free to browse and don't
-        hesitate to get in touch if you like what you see.
+        Junior Full-Stack Developer, based in Manchester, UK. My background is
+        in the Audio Industry, so I'm very interested in audio programming and
+        interactive media.
+      </p>
+      <h3>What is this site?</h3>
+      <p>
+        This page serves as my Software Development portfolio, so please feel
+        free to browse and don't hesitate to get in touch if you like what you
+        see.
+      </p>
+      <h3>Why does it look like a Cocktail Menu?</h3>
+      <p>
+        I built the site from the wireframe-upwards and experimented with this
+        style early-on. I liked the way it looked, so it stayed that way. I hope
+        you like it too, and that it makes you want an Old-Fashioned.
       </p>
     </>
   );
