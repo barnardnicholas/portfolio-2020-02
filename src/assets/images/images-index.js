434395e223af045676c586875264361dfc121f0f
Nick <barnard.nicholas@googlemail.com>
2020-02-16 22:10:48 +0000

    add thumbnail images for remaining portfolio pieces

--- src/assets/images/images-index.js
+++ src/assets/images/images-index.js
@@ -1,9 +1,15 @@
 import placeholder from "./placeholder-pic-300x100.png";
 import ntgtfoi from "./ntgtfoi_600x200.png";
+import tinydictionary from "./tinydictionary_600x200.png";
+import ambiscape from "./ambiscape_600x200.png";
+import ncnews from "./ncnews_600x200.png";
 
 const images = {
   placeholder: placeholder,
-  ntgtfoi: ntgtfoi
+  ntgtfoi: ntgtfoi,
+  ambiscape: ambiscape,
+  tinydictionary: tinydictionary,
+  ncnews: ncnews
 };
 
 export default images;
